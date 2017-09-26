ccbeb27a8dca7032db770a578cd47ce0344961f6
Kaitlyn Brouwers <kaitlynnicoleb@hotmail.com>
2017-09-26 18:03:06 -0400

    committing

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+
+module.exports = {
+  entry: ['./app/index.js'],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build')
+  },
+  module: {
+  	loaders: [
+  		{
+  			loader: 'babel-loader',
+  			test: /\.js$/,
+  			exclude: /node_modules/
+  		}
+  	]
+  },
+  devServer: {
+  	port: 3000, 
+  	contentBase: './build',
+  	inline: true
+  }
+}
